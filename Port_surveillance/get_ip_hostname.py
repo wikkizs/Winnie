7822d11fc12215ac4d8bc0949143deea6a6a5700
wikkizs <clementlavallee6@gmail.com>
2024-03-19 09:50:47 +0100

    Modifications de la surveillance de port

--- Port_surveillance/get_ip_hostname.py
+++ Port_surveillance/get_ip_hostname.py
@@ -1,17 +1,24 @@
 import socket
+import threading
 from Utils import server
 
 
 adresse_serveur = '192.168.1.26'
 port = 100
 
-
-def main() : 
-    port = 100 
-    server_socket = server.create_socket(adresse_serveur,port)
+def tcp_connexion(server_socket) : 
     client_socket , client_ip , client_hostname = server.accept_connexion(server_socket)
     print(f"{client_hostname} essaye de se connecter avec l'adresse IP {client_ip}")
     server_socket.close()
+
+def main() : 
+    while True : 
+        print("Test")
+        server_socket = server.create_socket(adresse_serveur,port)
+
+        thread_connexion = threading.Thread(target=tcp_connexion, args= (server_socket,))
+        thread_connexion.start()
+        thread_connexion.join()
 
 
 if __name__ == "__main__" : 
